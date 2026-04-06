fa43a7b6acdc6669c129b2786ca9c0fc17fa6780
joehsiao123 <joefw0211@gmail.com>
2026-04-06 16:52:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,18 @@ st.title("🍎 簡易健康紀錄器")
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # 讀取現有資料
+data = conn.read(spreadsheet=st.secrets["GSHEET_URL"], usecols=[0, 1, 2])
+data = data.dropna(how="all") # 清除空行
+
+# --- 輸入介面 ---
+with st.form("record_form", clear_on_submit=True):
+    date = st.date_input("日期", datetime.now())
+    category = st.selectbox("類別", ["飲食 (kcal)", "運動 (min)"])
+    item = st.text_input("內容 (例如: 雞腿便當 / 慢跑)")
+    value = st.number_input("數值", min_value=0)
+    
 #
+
+# --- 顯示最近 5 筆紀錄 ---
+st.write("### 📝 最近紀錄")
+st.table(data.tail(5))
